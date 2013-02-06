ac9ca8bbb26abf5e1a16acd554bc154bb9b528c0
evilncrazy <user2@server.fake>
2013-02-06 20:17:14 +1100

    Add bool, char and list data types

--- src/include/object.h
+++ src/include/object.h
@@ -9,11 +9,14 @@
 /* forward decl of environment */
 class SpEnv;
 
-typedef SpError *(*SpNative)(SpEnv *env, const int arity);
+typedef SpError *(*SpNative)(SpEnv *env);
 
 enum ObjectType {
    T_NULL, T_BOOL, T_INT, T_CHAR, T_DECIMAL, T_STRING, T_FUNCTION, T_REF, T_LIST 
 };
+
+/* forward decl of object */
+class SpObject;
 
 class SpFunction {
   private:
@@ -25,10 +28,28 @@ class SpFunction {
    SpFunction(const TokenIter begin, const TokenIter end);
 
    bool is_native() const { return native_code_; }
-   SpError *native_call(SpEnv *env, const int arity) const;
+   SpError *native_call(SpEnv *env) const;
 
    TokenIter bc_cbegin() const { return bytecode_.cbegin(); }
    TokenIter bc_cend() const { return bytecode_.cend(); }
+};
+
+class SpList {
+  private:
+   /* the underlying list */
+   std::vector<SpObject *> items_;
+  public:
+   SpList(); /* empty list */
+   SpList(const SpList &list);
+   SpList(const std::vector<SpObject *> items);
+   SpList(const std::vector<SpObject *>::iterator begin,
+          const std::vector<SpObject *>::iterator end);
+   SpList(const std::initializer_list<SpObject *> items); 
+
+   /* these method names corrospond to their respective Spooner functions */
+   int length() const { return items_.size(); }
+   void append(SpObject *obj) { items_.push_back(obj); }
+   SpObject *nth(const int index) const { return items_[index]; }
 };
 
 typedef union {
@@ -36,6 +57,7 @@ typedef union {
    double d;
    int n;
    SpFunction *f;
+   SpList *l;
 } Value;
 
 class SpObject {
@@ -47,14 +69,21 @@ class SpObject {
    SpObject(ObjectType type, Value val);
 
    static SpObject *create_int(const int value);
+   static SpObject *create_bool(const bool value);
+   static SpObject *create_char(const char value);
+   static SpObject *create_list(const SpList value);
    static SpObject *create_native_func(SpNative native);
 
+   SpObject *shallow_clone() const;
    SpObject *deep_clone() const;
 
    ObjectType type() const { return type_; }
    Value val() const { return v_; }
 
    int as_int() const { return v_.n; }
+   bool as_bool() const { return v_.n ? true : false; }
+   char as_char() const { return (char)v_.n; }
+   SpList *as_list() const { return v_.l; }
    SpFunction *as_func() const { return v_.f; }
 
    void print_self();
--- src/object.cpp
+++ src/object.cpp
@@ -2,9 +2,17 @@
 
 SpFunction::SpFunction(SpNative native) : is_native_(true), native_code_(native) { }
 
-SpError *SpFunction::native_call(SpEnv *env, const int arity) const {
-   return native_code_(env, arity);
+SpError *SpFunction::native_call(SpEnv *env) const {
+   return native_code_(env);
 }
+
+SpList::SpList() { }
+SpList::SpList(const SpList &list) : items_(list.items_) { }
+SpList::SpList(const std::vector<SpObject *> items) : items_(items) { }
+SpList::SpList(const std::vector<SpObject *>::iterator begin,
+               const std::vector<SpObject *>::iterator end) : 
+               items_(begin, end) { }
+SpList::SpList(const std::initializer_list<SpObject *> items) : items_(items) { }
 
 SpObject::SpObject(ObjectType type, bool default_val) : type_(type) {
    if (default_val) {
@@ -30,19 +38,50 @@ SpObject *SpObject::create_int(const int value) {
    return new SpObject(T_INT, int_val);
 }
 
+SpObject *SpObject::create_bool(const bool value) {
+   Value bool_val; bool_val.n = (int)value;
+   return new SpObject(T_BOOL, bool_val);
+}
+
+SpObject *SpObject::create_char(const char value) {
+   Value char_val; char_val.n = (int)value;
+   return new SpObject(T_CHAR, char_val);
+}
+
+SpObject *SpObject::create_list(const SpList value) {
+   Value list_val; list_val.l = new SpList(value);
+   return new SpObject(T_LIST, list_val);
+}
+
 SpObject *SpObject::create_native_func(SpNative native) {
    Value native_val; native_val.f = new SpFunction(native);
    return new SpObject(T_FUNCTION, native_val);
 }
 
+SpObject *SpObject::shallow_clone() const {
+   return new SpObject(type(), val());
+}
+
 SpObject *SpObject::deep_clone() const {
+   /* TODO: also clone non-value data (e.g. strings, lists, functions */
    return new SpObject(type(), val());
 }
 
 void SpObject::print_self() {
    switch (type()) {
-      case T_INT: printf("(INT: %d)\n", v_.n); break;
-      default: printf("(OBJECT)\n");
+      case T_INT: printf("%d", v_.n); break;
+      case T_CHAR: printf("'%c'", (char)v_.n); break;
+      case T_LIST: {
+         printf("[");
+         SpList *list = as_list();
+         for (int i = 0; i < list->length(); i++) {
+            if (i) printf(", ");
+            list->nth(i)->print_self();
+         }
+         printf("]");
+         break;
+      }
+      default: printf("(OBJECT)");
    }
 }
 
