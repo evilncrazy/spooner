fc4e110d86bf7b64e7c5e342d60fce6e2f01c58a
evilncrazy <user2@server.fake>
2013-02-06 20:20:35 +1100

    Add native functions for list processing

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,51 @@
 #include <iostream>
 #include <string>
 
+/* horrible macro for creating native functions through lambda syntax */
+#define NATIVE_FUNC(code) SpObject::create_native_func([] (SpEnv *env) -> SpError* { code return NO_ERROR; }) 
+
 void init_native_functions(SpEnv* base) {
    /* arithmetic functions */
-   base->bind_name("+", SpObject::create_native_func([] (SpEnv* env, const int arity) -> SpError* {
+   base->bind_name("+", NATIVE_FUNC(
       int sum = 0;
-      for (int i = 1; i <= arity; i++) {
-         SpObject *obj = env->resolve_name(std::string("$") + std::to_string(i));
+      SpList *args = env->resolve_name("$_")->as_list();
+      for (int i = 0; i < args->length(); i++) {
+         SpObject *obj = args->nth(i);
          if (obj && obj->type() == T_INT) sum += obj->as_int();
+         else return RUNTIME_ERROR_F("Argument %d cannot be added", i);
       }
       env->bind_name("$$", SpObject::create_int(sum));
-      return NO_ERROR;
-   }));
+   ));
+
+   /* core functions */
+
+   /* list processing */
+   base->bind_name("list", NATIVE_FUNC(
+      SpList *args = env->resolve_name("$_")->as_list();
+      env->bind_name("$$", SpObject::create_list(*args));
+   ));
+
+   /* length returns the length of any list */
+   base->bind_name("length", NATIVE_FUNC(
+      env->bind_name("$$", SpObject::create_int(env->resolve_name("$_")->as_list()->length()));
+   ));
+   
+   /* append concatenates its arguments together as a list */
+   base->bind_name("append", NATIVE_FUNC(
+      SpList *args = env->resolve_name("$_")->as_list();
+
+      SpList *head = args->nth(0)->as_list();
+      for (int i = 1; i < args->length(); i++) {
+         head->append(args->nth(i));
+      }
+      env->bind_name("$$", SpObject::create_list(*head));
+   ));
+
+   /* nth(i) gets the ith element of a list */
+   base->bind_name("nth", NATIVE_FUNC(
+      SpList *args = env->resolve_name("$_")->as_list();
+      env->bind_name("$$", args->nth(0)->as_list()->nth(args->nth(1)->as_int())->shallow_clone());
+   ));
 }
 
 int main(int argc, const char * argv[]) {
@@ -41,6 +75,9 @@ int main(int argc, const char * argv[]) {
          /* print the prompt and ask for input */
          printf("=> ");
          getline(std::cin, input);
+
+         /* check if this is a repl specific command */
+         if (input == ":quit") break;
 
          /* evaluate this line */
          char first = input[0];
@@ -79,6 +116,7 @@ int main(int argc, const char * argv[]) {
          if (vm.top_object()) {
             SpObject *result = vm.top_object();
             result->print_self();
+            printf("\n");
             vm.clear_objects();
          } else printf("NULL RETURN VALUE\n");
       }
