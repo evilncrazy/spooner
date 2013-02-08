e85d4559273fded3cfd03d5a0cabb9ca75c1b049
evilncrazy <user2@server.fake>
2013-02-08 22:09:44 +1100

    Implement name resolution of multimethods
    w/o patterns, pretty much useless, hence not used.

--- src/env.cpp
+++ src/env.cpp
@@ -2,30 +2,63 @@
 
 #include <cstdio>
 
+#include "include/pattern.h"
+
 SpEnv::~SpEnv() {
    /* free all the objects */
    while (!object_list_.empty()) {
-      /* this doesn't delete function objects,
-         hence memory leak if a function object is
-         allocated */
-      delete object_list_.back();
+      SpObject *back = object_list_.back();
       object_list_.pop_back();
+
+      if (back != NULL) {
+         delete back; back = NULL;
+      }
    }
 
    object_map_.clear();
 }
 
-void SpEnv::bind_name(const std::string name, SpObject *obj) {
-   object_map_.insert(std::make_pair(name, obj));
+void SpEnv::bind_name(const std::string name, SpObject *obj, const bool multi) {
+   std::string var_name = name;
+
+   /* now, we search for the next available name */
+   for (int i = 1; multi && object_map_.find(name) != object_map_.end(); i++) {
+      var_name = name + std::to_string(i);
+   }
+
+   object_map_.insert(std::make_pair(var_name, obj));
    object_list_.push_back(obj);
 }
 
 SpObject *SpEnv::resolve_name(const std::string name) {
    auto result = object_map_.find(name);
-   if (result != object_map_.end())
-      return result->second;
+   if (result != object_map_.end()) return result->second;
+
+   /* recursively search in parent environments */
+   return (depth() == 0) ? NULL : parent()->resolve_name(name);
+}
+
+SpObject *SpEnv::resolve_name(const std::string name, const SpObject *obj) {
+   auto result = object_map_.find(name);
+
+   if (result != object_map_.end()) {
+      if (result->second->type() == T_FUNCTION) {
+         if (SpMatch::match(result->second->as_func()->pattern(), obj).is_match()) {
+            return result->second;
+         } else {
+            /* we found a function with the same name, but the pattern does not
+               match our object, so we'll iterate through possible multimethods */
+            for (int i = 1; result = object_map_.find(name + std::to_string(i)), result != object_map_.end(); i++) {
+               /* check if the pattern matches */
+               if (SpMatch::match(result->second->as_func()->pattern(), obj).is_match()) { 
+                  return result->second;
+               }
+            }
+         }
+      } else return result->second;
+   }
    
    /* recursively search in parent environments */
    if (depth() == 0) return NULL; /* at the global scope */
-   else return parent()->resolve_name(name);
+   else return parent()->resolve_name(name, obj);
 }
